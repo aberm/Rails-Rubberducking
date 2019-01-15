b606ed6886512955c313abe834c7af04a66f81aa
aberm <abermendel@gmail.com>
2019-01-15 16:27:07 -0500

    Done.

--- app/controllers/ducks_controller.rb
+++ app/controllers/ducks_controller.rb
@@ -4,4 +4,38 @@ class DucksController < ApplicationController
     @ducks = Duck.all
   end
 
+  def show
+    @duck = Duck.find(params[:id])
+  end
+
+  def new
+    @duck = Duck.new
+    @students = Student.all
+  end
+
+  def create
+    @students = Student.all
+    @duck = Duck.create(strong_params(:name, :description, :student_id))
+    redirect_to duck_path(@duck)
+  end
+
+  def edit
+    @duck = Duck.find(params[:id])
+    @students = Student.all
+  end
+
+  def update
+    @duck = Duck.find(params[:id])
+    @duck.update(strong_params(:name, :description, :student_id))
+    @duck.save
+    redirect_to duck_path(@duck)
+  end
+
+  def delete
+  end
+
+  def strong_params(*args)
+    params.require(:duck).permit(*args)
+  end
+
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,4 +4,37 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def show
+    @student = Student.find(params[:id])
+    @ducks = @student.ducks
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.create(strong_params(:name, :mod))
+    @student.save
+    redirect_to student_path(@student)
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(strong_params(:name, :mod))
+    @student.save
+    redirect_to student_path(@student)
+  end
+
+  def delete
+  end
+
+  def strong_params(*args)
+    params.require(:student).permit(*args)
+  end
+
 end
